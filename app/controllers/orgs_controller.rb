a65deec606d6bda150f2e4527eb98142f482b628
Paul Mason <paul@northstarhc.com>
2012-10-17 18:33:22 -0600

    Added show for Orgs

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -3,5 +3,6 @@ class OrgsController < ApplicationController
     @orgs = Org.all 
   end
   def show
+    @org = Org.find(params[:id])
   end
 end
--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,10 +1,17 @@
 class PricesController < ApplicationController
+before_filter :find_project
    def index
 
       @prices = Price.where("org_id = '1179' and seq = '1'")
    end
+  def new
+    @price = @org.prices.build    
+  end
    def show
      @prices = Price.where("org_id = '1179'")
    end
-
+private
+  def find_org
+    @org = Org.find(params[:org_id])
+  end
 end
